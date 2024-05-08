7db4ee589d7cb2704010df4d7e375181503d1ed7
Shubham <shubham.bhardwaj@somaiya.edu>
2024-05-08 11:44:23 +0530

    Updated project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import "./common/styles/commonClasses.css";
+
+const Footer = () => {
+  return (
+    <div className="max-width footer-wrapper">
+    <div className="flex footer">
+      <div className="footer-text"><a href="https://shubham2903.netlify.app/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none' }}>Shubham.</a></div>
+      <div className="footer-icons">
+        <a href="https://www.linkedin.com/in/shubham-bhardwaj-ui-ux-designer/" target="_blank" rel="noopener noreferrer"><img src="assets/linkedin.svg" alt="LinkedIn" /></a>
+        <a href="https://join.skype.com/invite/ypX10Y4KidFH" target="_blank" rel="noopener noreferrer"><img src="assets/skype.svg" alt="Skype" /></a>
+        <a href="https://www.instagram.com/shubhambhardwaj257" target="_blank" rel="noopener noreferrer"><img src="assets/instagram.svg" alt="Instagram" /></a>
+      </div>
+    </div>
+      <div className="footer-details">
+        <div>Mob.No.: 9172405029</div>
+        <div>Email ID: bhardwajsb03.gmail.com</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/homePage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import Header from '../components/common/Header'
+import ProductShowcase from '../components/ProductShowcase'
+import FeelSpecial from '../components/FeelSpecial'
+import CredExperience from '../components/CredExperience'
+import CredSecurity from '../components/CredSecurity'
+import BrandsLove from '../components/BrandsLove'
+import WindowPeek from '../components/WindowPeek'
+import MobileScroll from '../components/MobileScroll'
+import CredStory from '../components/CredStory'
+import AppRating from '../components/AppRating'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <HeroSection /> 
+            <ProductShowcase/>
+            <FeelSpecial/>
+            <BrandsLove/>
+            <CredExperience/>
+            <MobileScroll/>
+            <div className="non-mobile">
+                <WindowPeek/>
+            </div>
+            <CredSecurity/>
+            <CredStory/>
+            <AppRating/>
+            <Footer/>   
+        </>
+    )
+}
+
+export default HomePage
